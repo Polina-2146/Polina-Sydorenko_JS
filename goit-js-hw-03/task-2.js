71d9066d2cf677275bddefa33c2ac0b57e2fd04b
polina <spolina062006@gmail.com>
2020-10-24 12:18:46 +0300

    hw3.task.2

--- goit-js-hw-03/task-2.js
+++ goit-js-hw-03/task-2.js
@@ -1,56 +1,34 @@
-// const calculateEngravingPrice = function (message, pricePerWord) {
-//     // твій код
-//   };
-  
-//   /*
-//    * Виклич функції для перевірки працездатності твоєї реалізації.
-//    */
-//   console.log(
-//     calculateEngravingPrice(
-//       'Proin sociis natoque et magnis parturient montes mus',
-//       10,
-//     ),
-//   ); // 80
-  
-//   console.log(
-//     calculateEngravingPrice(
-//       'Proin sociis natoque et magnis parturient montes mus',
-//       20,
-//     ),
-//   ); // 160
-  
-//   console.log(
-//     calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
-//   ); // 240
-  
-//   console.log(
-//     calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
-//   ); // 120
-
 
 
 
   const calculateEngravingPrice = function (message, pricePerWord) {
-    let count = 0;
-    let words = message.split(" ");
-    for (i = 0; i < words.length; i++) {
-      if (words[i] != "") {
-        count += 1;
-      }
-    }
-    console.log(count*pricePerWord);
-    };
-  
+    let price = message.split (" ");
+    return price.lenght * pricePerWord;
+  };
+  console.log (
   calculateEngravingPrice(
       'Proin sociis natoque et magnis parturient montes mus',
       10,
+  )
   );
   
-  calculateEngravingPrice(
-      'Proin sociis natoque et magnis parturient montes mus',
-      20,
-  );
+  console.log (
+    calculateEngravingPrice(
+        'Proin sociis natoque et magnis parturient montes mus',
+        20,
+    )
+    );
   
-      calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40);
+    console.log (
+      calculateEngravingPrice(
+          'Proin sociis natoque et magnis parturient montes mus',
+          40,
+      )
+      );
   
-      calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20);
+      console.log (
+        calculateEngravingPrice(
+            'Proin sociis natoque et magnis parturient montes mus',
+            20,
+        )
+        );
