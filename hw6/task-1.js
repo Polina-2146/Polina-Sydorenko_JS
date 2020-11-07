54eba583a12f03fc97324d895b61bc237af15ad0
polina <spolina062006@gmail.com>
2020-11-07 12:17:51 +0200

    hw6

--- hw6/task-1.js
+++ hw6/task-1.js
@@ -1,18 +1,18 @@
-// const user = {
-//     name: 'Mango',
-//     age: 20,
-//     hobby: 'html',
-//     premium: true,
-// };
+const user = {
+    name: 'Mango',
+    age: 20,
+    hobby: 'html',
+    premium: true,
+};
 
-// user.mood = 'happy';
-// user['hobby'] = 'skydiving';
-// user['premium'] = false;
-// console.log(user);
+user.mood = 'happy';
+user.hobby='skydiving';
+user.premium = false;
+console.log(user);
 
-// const keys = Object.keys(user);
-// const values = Object.values(user);
+const keys = Object.keys(user);
+const values = Object.values(user);
 
-// for (let key of keys) {
-//     console.log(${key}: ${user[key]});
-// }
+for (let key of keys) {
+    console.log(`${key}: ${user[key]}`);
+}
